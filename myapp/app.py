b4466bdf2baf3b2069354839066e669c69ea54dd
johnpfay <john.fay@duke.edu>
2024-05-03 12:47:31 -0400

    WIP

--- myapp/app.py
+++ myapp/app.py
@@ -1,7 +1,18 @@
 from shiny import App, render, ui
+import pandas as pd
+from pathlib import Path
+
+fn_LDAS = Path(__file__).parent / 'Peru_LDAS.csv'
+
+#df_LDAS = pd.read_csv(
+#    'Peru_LDAS.csv',
+#    #"https://github.com/johnpfay/ARC_shiny/raw/main/data/processed/Peru_LDAS.csv",
+#    dtype={'ubigeo':'str'},             #Read the ubigeo field as string
+#    parse_dates=['EpiweekStartDate']    #Read the EpiweekStartDate as datetime
+#).set_index(['ubigeo','EpiweekStartDate','year','week'])
 
 app_ui = ui.page_fluid(
-    ui.panel_title("Hello Shiny!"),
+    ui.panel_title(f"Hello Shiny GO! {fn_LDAS.exists()} "),
     ui.input_slider("n", "N", 0, 100, 20),
     ui.output_text_verbatim("txt"),
 )
